53a0d43d573d400d424f2ff12a1e8aadc56459a4
Juliano Silva <jbsilva.dev@outlook.com>
2025-05-10 15:10:44 -0300

    *

--- unlock_pdf/settings.py
+++ unlock_pdf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-!!e9%n_v*awc!gh@^9!hmb(#p8=j%)e0#&bms!-7-#5+0jc$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
